05ecea6e68d6ec618f0fd738f53a57d409ed8239
Sathira Kittisukmngkol <sathira.k@ku.th>
2017-02-19 18:18:42 +0700

    Add javadoc

--- src/coinpurse/BankNote.java
+++ src/coinpurse/BankNote.java
@@ -1,36 +1,67 @@
 package coinpurse;
 
+/**
+ * A Banknote with a monetary value and currency.
+ * @author Sathira Kittisukmongkol
+ */
 public class BankNote implements Valuable{
-	private double value;
+	//Value of the coin.
+	private final double value;
+	//Currency of the coin.
 	private String currency;
+	//Serialnumber of the banknote.
 	private long serialNumber;
+	//Set the serialnumber of banknote starts from 1,000,000.
 	private static long nextSerialNumber = 1_000_000;
 
+	/**
+	 * This is the constructor.
+	 * A banknote with given value using the default currency.
+	 * @param value , set the value of the banknote.
+	 */
 	public BankNote(double value){
 		this.value = value;
 		this.currency = "Bath";
 		this.serialNumber = nextSerialNumber++;
 	}
 	
+	/**
+	 * A banknote with given value and currency.
+	 * @param value , set the value to the banknote.
+	 * @param currency , set the currency to the banknote.
+	 */
 	public BankNote(double value,String currency){
 		this.value = value;
 		this.currency = currency;
 	}
 
+	/*
+	 * To get value of the banknote.
+	 */
 	@Override
 	public double getValue() {
 		return value;
 	}
 
+	/*
+	 * To get the currency of the banknote.
+	 */
 	@Override
 	public String getCurrency() {
 		return currency;
 	}
 
+	/*
+	 * To get the serialnumber of the banknote.
+	 */
 	public long getSerial(){
 		return serialNumber;
 	}
 
+	/*
+	 * To compare the banknote.
+	 * @param obj , the other banknote.
+	 */
 	public boolean equals(Object obj){
 		if(obj == null){
 			return false;
@@ -42,11 +73,19 @@ public class BankNote implements Valuable{
 		return ((other.getCurrency().equals(this.getCurrency()))&&(other.getValue()==this.getValue()));
 	}
 	
+	/*
+	 * To show the detail of the banknote.
+	 * @return sentence , the details of the banknote.
+	 */
 	public String toString(){
 		String ans = value+"-"+currency+" note ["+serialNumber+"]";
 		return ans;
 	}
 	
+	/*
+	 * CompareTo method is to compare the value of the banknote.
+	 * @param obj , obj is the object in BankNote class , we will compare its value.
+	 */
 	@Override
 	public int compareTo(Valuable o) {
 		if(o.getValue() > this.getValue()){
--- src/coinpurse/Valuable.java
+++ src/coinpurse/Valuable.java
@@ -1,14 +1,21 @@
 package coinpurse;
-/*
+
+/**
  * An interface for objects having a monetary value and currency.
+ * @author Sathira Kittisukmongkol
  */
 public interface Valuable extends Comparable<Valuable>{
+	
 	/*
 	 * Get the monetary value of this object , in its own currency.
 	 * @return the value of this object.
 	 */
 	public double getValue();
 	
+	/*
+	 * Get the monetary currency of this object , in its own currency.
+	 * @return the currency of this object.
+	 */
 	public String getCurrency();
 	
 }
